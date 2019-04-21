19cab85fb255587e84d96e58e14b431208b2d11d
fret2buzz <fret2buzz@hotmail.com>
2019-04-21 17:23:57 +0300

    enhancements
    enhancements

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sticky-elements
+
+```js
+$(document).ready(function(){
+    $(".box1").stickyElements({
+        top: 20,
+        container: '.sticky-container'
+    });
+    $(".box2").stickyElements({
+        top: 0,
+        container: '.sticky-container'
+    });
+});
+```
+`top` - position from top
+
+`container` - container to limit fixed position for sticky
--- js/sticky-elements.js
+++ js/sticky-elements.js
@@ -2,88 +2,96 @@
  *  stickyElements 2017
  *  Under MIT License
  */
-;( function( $, window, document, undefined ) {
+;
+(function($, window, document, undefined) {
 
-  "use strict";
+    "use strict";
 
     var pluginName = "stickyElements",
-      defaults = {
-        top: 20,
-        container: false
-      };
+        defaults = {
+            top: 20,
+            container: false,
+            zIndex: 'inherit'
+        };
 
-    function Plugin ( element, options ) {
-      this.element = element;
-      this.settings = $.extend( {}, defaults, options );
-      this._defaults = defaults;
-      this._name = pluginName;
-      this.init();
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
     }
 
-    $.extend( Plugin.prototype, {
-      init: function() {
-        var self = this;
-        this.el = $(this.element);
+    $.extend(Plugin.prototype, {
+        init: function() {
+            var self = this;
+            this.el = $(this.element);
 
-        this.el.wrapAll("<div class='sticky-elements' />");
-        this.el.wrapAll("<div class='sticky-elements-in' />");
+            this.el.wrapAll("<div class='sticky-elements' />");
+            this.el.wrapAll("<div class='sticky-elements-in' />");
 
-        this.stickyInner = this.el.parent();
-        this.sticky = this.stickyInner.parent();
-        this.container = this.sticky.parent();
+            this.stickyInner = this.el.parent();
+            this.sticky = this.stickyInner.parent();
+            this.container = this.sticky.parent();
 
-        this.top = this.settings.top;
+            this.top = this.settings.top;
+            this.zIndex = this.settings.zIndex;
 
-        if(this.settings.container){
-          this.container = $(this.settings.container);
-        }
-
-        this.setPos();
-
-        $(window).scroll(function(){
-            self.setPos();
-        });
-
-        $(window).resize(function(){
-            self.setPos();
-        });
-
-      },
-      setPos: function(){
-        this.pos = $(window).scrollTop();
-        this.containerHeight = this.container.height();
-        this.width = this.sticky.width();
-        this.height = this.stickyInner.height();
-
-        this.stickyInner.css("width", this.width + "px");
-
-        var topValue = this.sticky.offset().top - this.top;
-        var bottomValue = this.sticky.offset().top + this.containerHeight - this.height - this.top;
-
-        if (this.pos >= topValue && this.pos <= bottomValue) {
-            this.stickyInner.addClass('is-affixed');
-
-            if(this.pos <= bottomValue){
-                this.stickyInner.css('top', this.top + 'px');
+            if (this.settings.container) {
+                this.container = $(this.settings.container);
             }
-        } else {
-            this.stickyInner.removeClass('is-affixed');
-            this.stickyInner.css('top', (this.containerHeight - this.height) + 'px');
 
-            if(this.pos <= topValue){
-                this.stickyInner.css('top', 0);
+            this.setPos();
+
+            $(window).scroll(function() {
+                self.setPos();
+            });
+
+            $(window).resize(function() {
+                self.setPos();
+            });
+
+        },
+        setPos: function() {
+            this.scrollTopValue = $(window).scrollTop();
+            this.containerHeight = this.container.height();
+            this.stickyWidth = this.sticky.width();
+            this.stickyInner.css("width", this.stickyWidth + "px");
+
+            this.stickyInnerHeight = this.stickyInner.height();
+            this.marginTop = (this.containerHeight - this.stickyInnerHeight - (this.sticky.offset().top - this.container.offset().top));
+
+            this.topValue = this.sticky.offset().top - this.top;
+            this.bottomValue = this.container.offset().top + this.containerHeight - this.top - this.stickyInnerHeight;
+
+            if (this.scrollTopValue >= this.topValue && this.scrollTopValue <= this.bottomValue) {
+                this.stickyInner.addClass('is-affixed');
+                this.stickyInner.css("zIndex", this.zIndex);
+                this.sticky.css('height', this.stickyInnerHeight + 'px');
+
+                if (this.scrollTopValue <= this.bottomValue) {
+                    this.stickyInner.css('top', this.top + 'px');
+                }
+            } else {
+                this.stickyInner.removeClass('is-affixed');
+                this.stickyInner.css("zIndex", 'inherit');
+                this.sticky.css('height', 'auto');
+                this.stickyInner.css('top', this.marginTop + 'px');
+
+                if (this.scrollTopValue <= this.topValue) {
+                    this.stickyInner.css('top', 0);
+                }
             }
         }
-      }
     });
 
-    $.fn[ pluginName ] = function( options ) {
-      return this.each( function() {
-        if ( !$.data( this, "plugin_" + pluginName ) ) {
-          $.data( this, "plugin_" +
-            pluginName, new Plugin( this, options ) );
-        }
-      } );
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" +
+                    pluginName, new Plugin(this, options));
+            }
+        });
     };
 
-} )( jQuery, window, document );
+})(jQuery, window, document);
